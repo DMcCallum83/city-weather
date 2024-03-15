40d125b8f67f735299f0cd9b1bc8e41109cb44d8
David McCallum <david_mccallum83@yahoo.com>
2024-03-15 10:02:57 +0000

    remove old type

--- src/components/Search/_shared.ts
+++ src/components/Search/_shared.ts
@@ -1,23 +1,16 @@
-export interface SearchResult {
-    id: number;
-    name: string;
-    region: string;
-    country: string;
-  }
-
-  export interface CityResult {
-    name: string;
-    country: string;
-    localtime: string;
-    tempC: string;
-    feelsLikeC: string;
-    tempF: string;
-    feelsLikeF: string;
-    windMph: string;
-    windKph: string;
-    windDirection: string;
-    windDegrees: string;
-    condition: string;
-    conditionIcon: string;
-    humidity: string;
-  }
+export interface CityResult {
+  name: string;
+  country: string;
+  localtime: string;
+  tempC: string;
+  feelsLikeC: string;
+  tempF: string;
+  feelsLikeF: string;
+  windMph: string;
+  windKph: string;
+  windDirection: string;
+  windDegrees: string;
+  condition: string;
+  conditionIcon: string;
+  humidity: string;
+}
