688785d14b4df79262818337ebd451f823d320f9
David McCallum <david_mccallum83@yahoo.com>
2024-03-15 10:02:27 +0000

    use new type nad schema in api request code

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import {SearchResultSchema} from '../../schemas/searchSchema';
+import { z } from 'zod';
 
 export default async function handler(req, res) {
     const { query } = req.query;
@@ -8,9 +10,10 @@ export default async function handler(req, res) {
   
     try {
       const apiResponse = await fetch(fetchUrl);
-      const data = await apiResponse.json();
+      const json = await apiResponse.json();
+      const results = z.array(SearchResultSchema).parse(json);
   
-      res.status(200).json(data);
+      res.status(200).json(results);
     } catch (error) {
       console.error('Error fetching data:', error);
       res.status(500).json({ error: 'Internal Server Error' });
